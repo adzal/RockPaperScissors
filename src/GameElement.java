30181cf908138acf4ed6a72e68648ee966b0ca56
Adzal <adam.lockwood@orange.fr>
2022-06-01 11:20:15 +0200

    Removed unnecessary GameElement in stringToGameElement

--- src/GameElement.java
+++ src/GameElement.java
@@ -1,21 +1,22 @@
 /**
- * Game element enumeration
+ * Game element enumeration Rock, Paper or Scissors
  * @author adzal
  *
  */
 public enum GameElement {
 	ROCK, PAPER, SCISSORS;
+	
 	/**
-	 * Converts a string into a GameElement Enum
+	 * Converts a string into a GameElement Enumeration
 	 * @param humanChoice
 	 * @return GameElemnt of choice
 	 */
 	public static GameElement stringToGameElement(String humanChoice) {
 		GameElement choice = null;
 		switch (humanChoice) {
-		case "r", "R" -> choice = GameElement.ROCK;
-		case "p", "P" -> choice = GameElement.PAPER;
-		case "s", "S" -> choice = GameElement.SCISSORS;
+		case "r", "R" -> choice = ROCK;
+		case "p", "P" -> choice = PAPER;
+		case "s", "S" -> choice = SCISSORS;
 		}
 		return choice;
 	}
